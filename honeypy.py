924cfcaeb944044584732f2698c664a87ba8396c
MHT117 <53337905+MHT117@users.noreply.github.com>
2025-10-16 12:53:23 +0100

    added ssh server socket

--- honeypy.py
+++ honeypy.py
@@ -1,6 +1,8 @@
 # librarires 
 import logging
 from logging.handlers import RotatingFileHandler
+import socket
+import paramiko
 import os
 
 
@@ -22,5 +24,51 @@ creds_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
 creds_handler.setFormatter(logging_format)
 creds_logger.addHandler(funnel_handler)
 # emulate shell
+
+def emulate_shell(channel, client_ip):
+    channel.send(b'corporate-jumpbox2$ ')
+    command = b""
+    while True:
+        char = channel.recv(1)
+        channel.send(char)
+        if not char:
+            channel.close()
+        command += char
+        
+        if char == b'\r':
+            if command.strip() == b'exit':
+                channel.send(b'\n logout!\n')
+                channel.close()
+            elif command.strip() == b'pwd':
+                response = b"\n" + b'\\usr\\local' + b'\r\n'
+            elif command.strip() == b'whoami':
+                response = b"\n" + b"corpuser1" + b"\r\n"
+            elif command.strip() == b'ls':
+                response = b'\n' + b"jumpbox1.conf" + b"\r\n"
+            elif command.strip() == b'cat jumpbox1.conf':
+                response = b'\n' + b"Go to deeboodah.com" + b"\r\n"
+            else: 
+                response = b'\n' + bytes(command.strip()) + b"\r\n"
+        channel.send(response)
+        channel.send(b'corporate-jumpbox2$ ')
+        command = b""
 # ssh server + sockets
+
+class Server(paramiko.ServerInterface):
+    
+    def __init__(self, client_ip, input_username=None, input_password=None):
+        self.client_ip = client_ip
+        self.input_username = input_username
+        self.input_password = input_password
+    def check_channel_request(self, kind: str, chanid: int) -> int:
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+    def get_allowed_auths(self):
+        return "password"
+    def check_auth_password(self, username, password):
+        if self.input_username is not None and self.input_password is not None:
+            if username == 'username' and password == 'password':
+                return paramiko.AUTH_SUCCESSFUL
+            else:
+                return paramiko.AUTH_FAILED
 # provision ssh-based honeypot

6cb747a8c85bcd92363295d883b79c4cb64c9441
MHT117 <53337905+MHT117@users.noreply.github.com>
2025-10-18 14:33:14 +0100

    yeah so the hoenypot is done

--- honeypy.py
+++ honeypy.py
@@ -1,74 +1,43 @@
-# librarires 
-import logging
-from logging.handlers import RotatingFileHandler
-import socket
-import paramiko
-import os
+# Import library dependencies.
+import argparse
+# Import project python file dependencies. This is the main file to interface with the honeypot with.
+from ssh_honeypot import *
+from web_honeypot import *
+from dashboard_data_parser import *
+from web_app import *
 
-
-
-
-# constants
-logging_format = logging.Formatter('%(message)s')
-
-# loggers annd kogin files 
-funnel_logger = logging.getLogger('FunnelLogger')
-funnel_logger.setLevel(logging.INFO)
-funnel_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
-funnel_handler.setFormatter(logging_format)
-funnel_logger.addHandler(funnel_handler)
-
-creds_logger = logging.getLogger('FunnelLogger')
-creds_logger.setLevel(logging.INFO)
-creds_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
-creds_handler.setFormatter(logging_format)
-creds_logger.addHandler(funnel_handler)
-# emulate shell
-
-def emulate_shell(channel, client_ip):
-    channel.send(b'corporate-jumpbox2$ ')
-    command = b""
-    while True:
-        char = channel.recv(1)
-        channel.send(char)
-        if not char:
-            channel.close()
-        command += char
-        
-        if char == b'\r':
-            if command.strip() == b'exit':
-                channel.send(b'\n logout!\n')
-                channel.close()
-            elif command.strip() == b'pwd':
-                response = b"\n" + b'\\usr\\local' + b'\r\n'
-            elif command.strip() == b'whoami':
-                response = b"\n" + b"corpuser1" + b"\r\n"
-            elif command.strip() == b'ls':
-                response = b'\n' + b"jumpbox1.conf" + b"\r\n"
-            elif command.strip() == b'cat jumpbox1.conf':
-                response = b'\n' + b"Go to deeboodah.com" + b"\r\n"
-            else: 
-                response = b'\n' + bytes(command.strip()) + b"\r\n"
-        channel.send(response)
-        channel.send(b'corporate-jumpbox2$ ')
-        command = b""
-# ssh server + sockets
-
-class Server(paramiko.ServerInterface):
+if __name__ == "__main__":
+    # Create parser and add arguments.
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('-a','--address', type=str, required=True)
+    parser.add_argument('-p','--port', type=int, required=True)
+    parser.add_argument('-u', '--username', type=str)
+    parser.add_argument('-w', '--password', type=str)
+    parser.add_argument('-s', '--ssh', action="store_true")
+    parser.add_argument('-t', '--tarpit', action="store_true")
+    parser.add_argument('-wh', '--http', action="store_true")
     
-    def __init__(self, client_ip, input_username=None, input_password=None):
-        self.client_ip = client_ip
-        self.input_username = input_username
-        self.input_password = input_password
-    def check_channel_request(self, kind: str, chanid: int) -> int:
-        if kind == 'session':
-            return paramiko.OPEN_SUCCEEDED
-    def get_allowed_auths(self):
-        return "password"
-    def check_auth_password(self, username, password):
-        if self.input_username is not None and self.input_password is not None:
-            if username == 'username' and password == 'password':
-                return paramiko.AUTH_SUCCESSFUL
-            else:
-                return paramiko.AUTH_FAILED
-# provision ssh-based honeypot
+    args = parser.parse_args()
+    
+    # Parse the arguments based on user-supplied argument.
+    try:
+        if args.ssh:
+            print("[-] Running SSH Honeypot...")
+            honeypot(args.address, args.port, args.username, args.password, args.tarpit)
+
+        elif args.http:
+            print('[-] Running HTTP Wordpress Honeypot...')
+            #if args.nocountry:
+                #pass_country_status(True)
+            if not args.username:
+                args.username = "admin"
+                print("[-] Running with default username of admin...")
+            if not args.password:
+                args.password = "deeboodah"
+                print("[-] Running with default password of deeboodah...")
+            print(f"Port: {args.port} Username: {args.username} Password: {args.password}")
+            run_app(args.port, args.username, args.password)
+        else:
+            print("[!] You can only choose SSH (-s) (-ssh) or HTTP (-h) (-http) when running script.")
+    except KeyboardInterrupt:
+        print("\nProgram exited.")
